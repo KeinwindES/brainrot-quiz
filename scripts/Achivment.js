1168095b72f6e0ec2f511e6377c310434e65c01f
Keinwind <KeinwindEichenSchwert@proton.me>
2025-11-19 12:42:39 +0100

    Add quiz features with validation, scoring, and audio feedback

---
+++ scripts/Achivment.js
@@ -0,0 +1,2 @@
+const Locked = [];
+const UnLocked = [];
---
+++ scripts/Sound.js
@@ -0,0 +1,6 @@
+const ss = new Audio("../SFX/67_SQlv2Xv.mp3");
+const aps = new Audio("../SFX/apple-pay-sound.mp3");
+const st = new Audio("../SFX/skibidi-toilet.mp3");
+const soyg = new Audio("../SFX/sticking-out-your-gyatt_8pkoYt5.mp3");
+const totb = new Audio("../SFX/thick-of-it-brainrot.mp3");
+const vts = new Audio("../SFX/villager-tung-sahur.mp3");
